47a8e0204a603f25f6d21a41522c37c0d042b891
Shivam Mishra <bt24cse020@nituk.ac.in>
2025-02-19 16:16:54 +0000

    Navbar created

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-
+import Navbar from './Navbar/Navbar.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="bg-yellow p-4 text-center text-2xl text-red-500 font-bold mx-auto mt-4 w-1/2">
-        Hello from College Hub Frontend!
+      <div>
+        <Navbar/>
       </div>
     </>
   )
